cf7f6812206d28287bbfdd08bc0b67f150b71cd8
catwrightb <78883365+catwrightb@users.noreply.github.com>
2022-04-04 00:48:03 +1200

    add pages folder and redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
 import React from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
 
-// import Home from "./pages/Home";
-// import SEPractice from "./pages/SE-Practice";
-// import SubmitArticle from "./pages/Submit-Article"; 
-// import NotFoundPage from "./pages/404";
-
-const App = () =>  {
-    return (
-        <div>
-          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-            <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
-            </ul>
-          <div className="content">
-          </div>
+const App = () => {
+  return (
+    <Router >
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice" >Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle" >Submit an Article</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            {/* All other paths should redirect to NotFoundPage */}
+            <Route exact path="404" element={<NotFoundPage />} />
+            <Route path="/*" element={<Navigate to="404" replace />} />
+          </Routes>
         </div>
-    );
-}
+      </div>
+    </Router>
+  );
+};
  
 export default App;
